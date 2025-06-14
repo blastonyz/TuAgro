eff4805bfdabbfb2ecb258c1deee0463f6411fe1
blastonyz <blastonyzamora@gmail.com>
2025-06-14 03:31:14 -0300

    latest front

--- tuagro-front/app/components/nosotros/UsSection.jsx
+++ tuagro-front/app/components/nosotros/UsSection.jsx
@@ -24,25 +24,25 @@ const UsSection = () => {
                         Somos una organización comercial que provee de insumos y soluciones agrícolas de manera rápida eficaz y segura a lo largo de todo el territorio nacional.
                     </p>
 
-                   <div className='usTitles'>
+                    <div className='usTitles'>
                         <h3>
                             Asesoramiento Técnico a medida
-                           
-                        </h3> 
-                        <CheckCircle/>
-                   </div>
+
+                        </h3>
+                        <CheckCircle />
+                    </div>
 
                     <p>
                         Ofrecemos acompañamiento personalizado en cada etapa del ciclo productivo. Nuestro objetivo es brindar no solo productos de alta calidad, sino también estrategias integrales que permitan al productor optimizar costos de explotación sin comprometer la eficiencia en el control de plagas, enfermedades y malezas.
                     </p>
 
-   <div className='usTitles'>
+                    <div className='usTitles'>
                         <h3>
                             Amplia gama de productos agroquímicos
-                                
+
                         </h3>
-     <CheckCircle/>
-   </div>
+                        <CheckCircle />
+                    </div>
                     <p>Contamos con un completo portafolio de insumos, que incluye:</p>
 
                     <p>
@@ -69,14 +69,14 @@ const UsSection = () => {
                     <AnimatedProgress value={93} color={colors.green} />
 
 
-       <div className='ustitles'>
+                    <div className='ustitles'>
                         <h3>
                             Representantes oficiales de Bloemen
-                                
-                        </h3> 
-                        <CheckCircle/>
-    
-       </div>
+
+                        </h3>
+                        <CheckCircle />
+
+                    </div>
                     <p>
                         Somos distribuidores autorizados de la línea Bloemen, lo que nos permite ofrecer tecnologías de vanguardia y soluciones confiables para una agricultura más eficiente y sustentable.
                     </p>
