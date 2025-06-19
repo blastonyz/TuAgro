482ef6909989a5b161b3f1642c46b1a54c3bc705
blastonyz <blastonyzamora@gmail.com>
2025-06-19 13:50:18 -0300

    trying to fix editproduct2

--- tuagro-front/app/auth/@admin/edit/[_id]/page.js
+++ tuagro-front/app/auth/@admin/edit/[_id]/page.js
@@ -1,10 +1,9 @@
-'use client'
 import '../../../../components/admin/edit/edit.form.css'
 import EditForm from '@/app/components/admin/edit/EditForm'
 
 export default async function Edit({ params }) {
-  const { _id } = await params; 
-  const res = await fetch(`/api/product/${_id}`);
+  const { _id } =  params; 
+  const res = await fetch(`${process.env.NEXT_PUBLIC_RENDER_API_URL}/product/${_id}`);
   const product = await res.json()
 
   return (
