04d01dc5b2375f401e694a1b4af5d29abdc23301
blastonyz <blastonyzamora@gmail.com>
2025-07-13 23:08:39 -0300

    tryn to fix auth cookies2

--- tuagro-front/app/auth-redirect/page.js
+++ tuagro-front/app/auth-redirect/page.js
@@ -1,12 +1,11 @@
 'use client'
 
 import { useEffect } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter} from 'next/navigation'
 import { useAuthContext } from '../components/context/AuthContext'
 
 export default async function OAuthCallback({params}) {
     const router = useRouter()
-    const params = useSearchParams()
     const {verifyUser} = useAuthContext()
     const {token} = await params
 
