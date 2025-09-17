4dc9bef2e039ba5e70677bf92c058837209eedc5
blastonyz <blastonyz@gmail.com>
2025-09-17 17:54:04 -0300

    sizes in db model

--- BackEnd-TuAgro/src/models/products.model.js
+++ BackEnd-TuAgro/src/models/products.model.js
@@ -8,8 +8,8 @@ export const ProductSchema = new mongoose.Schema({
     price: { type: Number, required: true },
     shortDescription: { type: String, required: true },
     stock: { type: Number, required: true },
-    title: { type: String, required: true }
-
+    title: { type: String, required: true },
+    size: { type: [String], default : ["20","1000"]}
 },
     { timestamps: true }
 )
